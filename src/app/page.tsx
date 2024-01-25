6240ab41d389551f9c3d275bebcf963bb7021fff
wallison da silva gregorio <wallisongregorio2022@gmail.com>
2024-01-25 11:22:11 -0300

    inicio do projeto pomodoro

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+//components
+import ExperienceBar from "../components/ExperienceBar";
+
+export default function Home() {
+  return (
+    <div className="App">
+      <ExperienceBar/>
+    </div>
+  );
+}
